da83f353d6a2f4b7b8fa68b607e6aa04e67b513b
jaimehrubiks <jaimehidalgo2@hotmail.com>
2017-04-09 19:33:29 +0200

    twitter example working

---
+++ nodejs/twitter_notifier/index.js
@@ -0,0 +1,26 @@
+/* Imports */
+var Twit            = require('twit')
+var twit_options    = require('./twitter_auth.json');
+var notmail_options = require('./notmail_auth.json');
+var request         = require('request');
+
+/* Options */
+twit_options.timeout_ms = 60*1000;
+var msg = { dest: {user: notmail_options.user}, msg: {} }
+
+/* Main */
+var T = new Twit(twit_options)
+var stream = T.stream('statuses/filter', { track: '#edsheeran' })
+stream.on('tweet', function (tweet) {
+    let newTweet = `New #notMAIL Tweet!!
+    * Autor del Tweet: ${tweet.user.name} (@${tweet.user.screen_name})
+    * Tweet: ${tweet.text}
+    * URL: https://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}`
+    msg.msg.title = 'Nuevo tweet de @' + tweet.user.screen_name + "!"
+    msg.msg.data = newTweet;
+    request.post({
+        url: notmail_options.url+'/app/msg/?unique_id='+notmail_options.unique_id+'&shared_key='+notmail_options.shared_key,
+        json: true,
+        body: msg
+    }, function(data){ })
+})
