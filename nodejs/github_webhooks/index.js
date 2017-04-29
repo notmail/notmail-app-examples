c633d61b057d2dd45f2d6906501a47ea2a046f8e
jaimehrubiks <jaimehidalgo2@hotmail.com>
2017-04-29 18:16:54 +0200

    github webhooks funcionando

--- nodejs/github_webhooks/index.js
+++ nodejs/github_webhooks/index.js
@@ -1,8 +1,15 @@
-var app = require('express')();
+var app             = require('express')(),
+    request         = require('request');
+    notmail_options = require('./notmail_auth.json'),
+    msg             = { dest: {user: notmail_options.user}, msg: {} }
 
 app.use(require('body-parser').json());
 app.post('/', function (req, res) {
+    msg.msg.title = 'Nueva respuesta de @' + req.body.comment.user.login + " en '" + req.body.issue.title + "' !";
+    msg.msg.data = req.body.comment.user.login + " escribió: \n\n" + req.body.comment.body + "\n\nVer en: " + req.body.issue.html_url;
     res.status(200).end();
+    request.post({  url: notmail_options.url+'/app/msg/?unique_id='+notmail_options.unique_id+'&shared_key='+notmail_options.shared_key, 
+                    json: true, body: msg }, function(data){ })
 });
 
 app.listen(4321, function () { });
